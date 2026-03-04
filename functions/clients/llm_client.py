aeb4ff2200c086fd4e09c847bcb282fb4c1e94c6
Anthony White <ajw4747@gmail.com>
2026-03-04 17:56:36 -0500

    Removed LLM_API_URL because using Anthronpic API instead.

--- functions/clients/llm_client.py
+++ functions/clients/llm_client.py
@@ -1,14 +1,9 @@
 import os
-# import ollama
-
 import anthropic
 
 
 class LLMClient:
     def __init__(self):
-        # self._url = os.getenv("LLM_API_URL")
-        # self._client = ollama.Client(host=self._url)
-
         self._client = anthropic.Anthropic(
             api_key=os.getenv("ANTHROPIC_API_KEY"))
 
