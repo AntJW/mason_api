d30aaf7bbda2e7f1827ca86d491e104a29406d50
Anthony White <ajw4747@gmail.com>
2026-03-25 21:55:59 -0400

    Updated llm client.

--- functions/clients/llm_client.py
+++ functions/clients/llm_client.py
@@ -1,5 +1,8 @@
 import os
 import anthropic
+import json
+from anthropic._types import NOT_GIVEN
+from anthropic.types import OutputConfigParam
 
 
 class LLMClient:
@@ -10,3 +13,26 @@ class LLMClient:
     @property
     def client(self):
         return self._client
+
+    def create_message(self,  system: str, messages: list[dict], output_config: OutputConfigParam | None = None, model: str = os.getenv("LLM_MODEL"), max_tokens: int = 4096):
+        response = self._client.messages.create(
+            model=model,
+            max_tokens=max_tokens,
+            system=system,
+            messages=messages,
+            output_config=output_config if output_config is not None else NOT_GIVEN
+        )
+        return response.content[0].text
+
+    def stream_message(self, system: str, messages: list[dict], model: str = os.getenv("LLM_MODEL"), max_tokens: int = 4096):
+        with self._client.messages.stream(
+            model=model,
+            max_tokens=max_tokens,
+            system=system,
+            messages=messages
+        ) as stream:
+            for text in stream.text_stream:
+                if text:
+                    yield json.dumps(
+                        {"role": "assistant", "content": text}
+                    )
