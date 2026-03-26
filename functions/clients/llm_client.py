06a4262334560b2717b5e8681a295442e5d70baf
Anthony White <ajw4747@gmail.com>
2026-03-25 22:17:26 -0400

    Added .parse() for future use case.

--- functions/clients/llm_client.py
+++ functions/clients/llm_client.py
@@ -3,6 +3,8 @@ import anthropic
 import json
 from anthropic._types import NOT_GIVEN
 from anthropic.types import OutputConfigParam
+from pydantic import BaseModel
+from typing import Any
 
 
 class LLMClient:
@@ -36,3 +38,25 @@ class LLMClient:
                     yield json.dumps(
                         {"role": "assistant", "content": text}
                     )
+
+    # Parse the response into json output format based on the Pydantic model
+    def parse_message(self,  system: str, messages: list[dict], output_format: BaseModel, model: str = os.getenv("LLM_MODEL"), max_tokens: int = 4096):
+        response = self._client.messages.parse(
+            model=model,
+            max_tokens=max_tokens,
+            system=system,
+            messages=messages,
+            # .parse() still accept output_format as a convenience parameter. The SDK handles the translation to output_config.format internally for Pydantic models
+            output_format=output_format
+        )
+        return response.content[0].text
+
+
+# # TODO: Use these Pydantic models to ensure response that requires Quill Delta format are returned in the correct format
+# # Pydantic models for Quill Delta
+# class DeltaOp(BaseModel):
+#     insert: str | dict
+#     attributes: dict[str, Any] | None = None
+#
+# class QuillDelta(BaseModel):
+#     ops: list[DeltaOp]
